7968239a4d07d842bc0e0c87c80aab49934d8a47
qitianfeng <1454429051@qq.com>
2020-06-07 22:50:37 +0800

    前台商品查询

--- mall-goods/mall-goods-service/src/main/java/com/mmall/goods/GoodsServiceApplication.java
+++ mall-goods/mall-goods-service/src/main/java/com/mmall/goods/GoodsServiceApplication.java
@@ -4,12 +4,13 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableSwagger2
-@MapperScan("com.mmall.goods.mapper")
+@ComponentScan(basePackages = {"com.mmall"})
 public class GoodsServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(GoodsServiceApplication.class,args);
